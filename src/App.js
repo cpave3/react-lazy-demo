7fbf69705b85ff14a41cdc253067bcd2f7d2f3ba
Cameron Pavey <cpave3@gmail.com>
2023-05-07 22:24:36 +1000

    add route-based code splitting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import React from "react";
-// import Date from "./components/Date";
-// import CatFact from "./components/CatFact";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
 const CatFact = React.lazy(() => import("./components/CatFact"));
 const Date = React.lazy(() => import("./components/Date"));
 
-function App() {
+function Layout() {
   return (
     <div>
-      <div>
-        date: <Date />
-      </div>
-      <div>
-        cat fact: <CatFact />
-      </div>
+      <h1>Home</h1>
+      <ul>
+        <li>
+          <Link to="date">Date</Link>
+        </li>
+        <li>
+          <Link to="cat-fact">Cat Fact</Link>
+        </li>
+      </ul>
+      <Outlet />
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route
+          path="date"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <Date />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="cat-fact"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <CatFact />
+            </React.Suspense>
+          }
+        />
+        <Route path="*" element={<div>nothing</div>} />
+      </Route>
+    </Routes>
   );
 }
 
